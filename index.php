65c3ca6b3ee532b343b57bf0d4b5ce0c136ab223
fasol <remi.parrot@gmail.com>
2016-12-22 15:34:16 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Pouce d'Or : Le site officiel !</title>
+	<base href="/">
+
+	<!-- Balise meta pour Google (textes ...) -->
+	<meta name="description" content="Le Pouce d'Or, c'est LE concours de stop des écoles d'ingénieurs, de commerce et des établissements d'enseignement supérieur. 
+	
+	Le but : Aller le plus loin possible et revenir en stop en 48h.
+	
+	Tu te sens l'âme d'aventurier : prends ton sac, trouve ton équipier et c'est partie !" />
+	
+	<!-- Balise meta pour une bonne apparition sur Facebook -->
+	<meta property= "og:title" content= "Pouce d'Or : Le site officiel !" />
+	<meta property= "og:description" content= "Le concours de stop des écoles d'ingénieurs, de commerce et d'éducation supérieurs. 
+	Le but : Aller le plus loin possible et revenir en stop en 48h." />
+	<meta property="og:image" content="http://www.poucedor.fr/images/miniature_fb.jpg" />
+	<meta property="og:url" content="http://www.poucedor.fr" />
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/x-icon" href="favicon.ico">
+
+	<!-- Material Icons -->
+	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link href="materialize/css/materialize.min.css" rel="stylesheet" type="text/css" media="screen,projection"/>
+
+	<!-- global stylesheet -->
+	<link href="index.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<!--Import jQuery before materialize.js-->
+	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+
+	<?php include "app/app.component.php"; ?>
+</body>
+</html>
